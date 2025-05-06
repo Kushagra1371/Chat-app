8eff27461cec6a7bb5252bb9776ac1077db1b0a8
Kushagra1371 <116947438+Kushagra1371@users.noreply.github.com>
2025-05-06 23:58:30 +0530

    Update message.controller.js

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -28,16 +28,11 @@ export const sendMessage = async (req, res) => {
 			conversation.messages.push(newMessage._id);
 		}
 
-		// await conversation.save();
-		// await newMessage.save();
 
-		// this will run in parallel
 		await Promise.all([conversation.save(), newMessage.save()]);
 
-		// SOCKET IO FUNCTIONALITY WILL GO HERE
 		const receiverSocketId = getReceiverSocketId(receiverId);
 		if (receiverSocketId) {
-			// io.to(<socket_id>).emit() used to send events to specific client
 			io.to(receiverSocketId).emit("newMessage", newMessage);
 		}
 
@@ -56,6 +51,7 @@ export const getMessages = async (req, res) => {
 		const conversation = await Conversation.findOne({
 			participants: { $all: [senderId, userToChatId] },
 		}).populate("messages"); 
+
 		if (!conversation) return res.status(200).json([]);
 
 		const messages = conversation.messages;
