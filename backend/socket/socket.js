ffc2062ffd89bc3fb2ccab2705fdd9a2c77355b3
Kushagra1371 <116947438+Kushagra1371@users.noreply.github.com>
2025-05-07 00:01:50 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -16,7 +16,7 @@ export const getReceiverSocketId = (receiverId) => {
 	return userSocketMap[receiverId];
 };
 
-const userSocketMap = {}; // {userId: socketId}
+const userSocketMap = {}; 
 
 io.on("connection", (socket) => {
 	console.log("a user connected", socket.id);
@@ -24,10 +24,8 @@ io.on("connection", (socket) => {
 	const userId = socket.handshake.query.userId;
 	if (userId != "undefined") userSocketMap[userId] = socket.id;
 
-	
 	io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-	
 	socket.on("disconnect", () => {
 		console.log("user disconnected", socket.id);
 		delete userSocketMap[userId];
